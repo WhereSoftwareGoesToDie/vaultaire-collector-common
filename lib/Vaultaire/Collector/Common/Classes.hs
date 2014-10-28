aa708b9bc9030b8b7bf9d7e152721cfe604f5d46
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-10-28 13:32:16 +1100

    Change from query to fourtuples for now

--- lib/Vaultaire/Collector/Common/Classes.hs
+++ lib/Vaultaire/Collector/Common/Classes.hs
@@ -8,10 +8,9 @@ module Vaultaire.Collector.Common.Classes where
 import           Options.Applicative
 
 import           Vaultaire.Collector.Common.Types
-import           Vaultaire.Query
 
 class Monad m => CollectorMonad o s m | m -> o, m -> s where
     --Give us a way to parse extra options, a program description and a header
     parseExtraOpts       :: m (Parser o, String, String)
     initialiseExtraState :: CollectorOpts o -> m s
-    collect              :: Query (Collector o s m) (Either (Address, SourceDict) SimplePoint)
+    collectFourTuple     :: Collector o s m FourTuple
