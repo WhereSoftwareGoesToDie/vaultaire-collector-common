02bedffa3f0ecb11f71860df3e3941c36d422d68
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-11-20 10:39:14 +1100

    Added runNullCollector + minor refactoring

--- lib/Vaultaire/Collector/Common/Process.hs
+++ lib/Vaultaire/Collector/Common/Process.hs
@@ -4,6 +4,7 @@
 module Vaultaire.Collector.Common.Process
     ( runBaseCollector
     , runCollector
+    , runNullCollector
     , collectSource
     , collectSimple
     ) where
@@ -15,6 +16,7 @@ import           Options.Applicative
 import           System.Log.Logger
 
 import           Marquise.Client
+import           Marquise.Types
 import           Vaultaire.Types
 
 import           Vaultaire.Collector.Common.Types
@@ -37,18 +39,48 @@ runCollector :: MonadIO m
              -> Collector o s m a
              -> m a
 runCollector parseExtraOpts initialiseExtraState cleanup collect = do
-    (cOpts, eOpts) <- liftIO $
+    (opts, st) <- setup parseExtraOpts initialiseExtraState getInitialCommonState
+    runCollector' opts st cleanup collect
+
+-- | Run a Vaultaire Collector which outputs to /dev/null
+--   Suitable for testing
+runNullCollector :: MonadIO m
+                 => Parser o
+                 -> (CollectorOpts o -> m s)
+                 -> Collector o s m ()
+                 -> Collector o s m a
+                 -> m a
+runNullCollector parseExtraOpts initialiseExtraState cleanup collect = do
+    (opts, st) <- setup parseExtraOpts initialiseExtraState getNullCommonState
+    runCollector' opts st cleanup collect
+
+-- | Helper run function
+runCollector' :: Monad m
+              => CollectorOpts o
+              -> CollectorState s
+              -> Collector o s m ()
+              -> Collector o s m a
+              -> m a
+runCollector' opts st cleanup collect =
+    let collect' = unCollector $ do
+            result <- collect
+            cleanup
+            return result
+    in evalStateT (runReaderT collect' opts) st
+
+-- | Helper function to setup initial state of the collector
+setup :: MonadIO m
+      => Parser o
+      -> (CollectorOpts o -> m s)
+      -> (CommonOpts -> m CommonState)
+      -> m (CollectorOpts o, CollectorState s)
+setup parseExtraOpts initialiseExtraState initialiseCommonState = do
+    opts@(cOpts, _) <- liftIO $
         execParser (info (liftA2 (,) parseCommonOpts parseExtraOpts) fullDesc)
     liftIO $ setupLogger (optLogLevel cOpts)
-    let opts = (cOpts, eOpts)
-    cState <- getInitialCommonState cOpts
+    cState <- initialiseCommonState cOpts
     eState <- initialiseExtraState opts
-    evalStateT (runReaderT (unCollector collect') opts) (cState, eState)
-  where
-    collect' = do
-        result <- collect
-        cleanup
-        return result
+    return (opts, (cState, eState))
 
 -- | Sets the global logger to the given priority
 setupLogger :: Priority -> IO ()
@@ -66,6 +98,13 @@ getInitialCommonState CommonOpts{..} = do
         (\e -> error $ "Error creating spool files: " ++ show e)
         (createSpoolFiles optNamespace)
     return $ CommonState files emptySourceCache
+
+-- | Generates a dummy set of spool files and an empty SourceDictCache
+getNullCommonState :: MonadIO m
+                    => CommonOpts
+                    -> m CommonState
+getNullCommonState CommonOpts{..} =
+    return $ CommonState (SpoolFiles "/dev/null" "/dev/null") emptySourceCache
 
 -- | Wrapped Marquise.Client.queueSourceUpdate with logging and caching
 collectSource :: MonadIO m => Address -> SourceDict -> Collector o s m ()
